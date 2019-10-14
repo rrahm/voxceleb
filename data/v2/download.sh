2dc364c344abbba08294880a31eef7d65b7677ae
rrahm <roman-rahm@hotmail.de>
2019-10-14 16:45:40 +0200

    Update download.sh

--- data/v2/download.sh
+++ data/v2/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #fill in your credentials
-USERNAME="voxceleb000"
-PASS="xxxxxxx"
+USERNAME="voxceleb1904"
+PASS="9hmp7488"
 
 wget --user ${USERNAME} --password ${PASS}  -i download_files.list
